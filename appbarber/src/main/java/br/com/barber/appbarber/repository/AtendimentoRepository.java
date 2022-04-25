0392093e74e25dcd0a7c304c7a90c5c2dcb43b32
Jhonatan Vinicios Alves de Oliveira <jvaoliveira@sefaz.se.gov.br>
2022-04-25 07:57:00 -0300

    deletendo duplicatas

--- appbarber/src/main/java/br/com/barber/appbarber/repository/AtendimentoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.barber.appbarber.repository;
-
-import br.com.barber.appbarber.models.Atendimento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AtendimentoRepository extends JpaRepository<Atendimento, Integer> {
-}
--- appbarber/src/main/java/br/com/barber/appbarber/repository/UsuarioRepository.java
+++
@@ -1,6 +0,0 @@
-package br.com.barber.appbarber.repository;
-
-import br.com.barber.appbarber.models.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{}
